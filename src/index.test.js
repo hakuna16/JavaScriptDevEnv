31c08f7bafe389da0cdb6560e37328690ac31119
Rituj Kumar <ritujkumar@hotmail.com>
2017-12-05 12:56:42 +0530

    ading files related to http request fetch

--- src/index.test.js
+++ src/index.test.js
@@ -9,11 +9,11 @@ describe('Our first test', () => {
 });
 
 describe('Our Index.html Test', () => {
-  it('Should say hello', (done) => {
+  it('Should say Users', (done) => {
     const index = fs.readFileSync('./src/index.html', 'utf-8');
     jsdom.env(index, function (err, window) {
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal('Hello World');
+      expect(h1.innerHTML).to.equal('Users');
       done();
       window.close();
     });
